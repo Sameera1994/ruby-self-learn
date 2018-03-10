259c9d810782d687f271236d23afb6f0f42919ff
Sameera Lakshitha <lakshithasameera89@gmail.com>
2018-03-10 23:03:01 +0530

    operators
    + addition      
      - subtraction      
      * multiplication  
      / division

---
+++ numbers.rb
@@ -0,0 +1,16 @@
+
+=begin  
+ Ruby Numbers  
+ Usual operators:  
+  + addition      
+  - subtraction      
+  * multiplication  
+  / division  
+=end  
+  
+puts 3 + 5  # prints  8
+puts 2 * 3  # prints  6
+# Integer division  
+# When you do arithmetic with integers, you'll get integer answers  
+puts 5 / 2   # prints  2
+puts 5 - 7   # prints  -2
