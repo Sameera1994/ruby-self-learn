a3eede56a1ecc086ef0b9ba359aa8547f0ad2993
Sameera Lakshitha <lakshithasameera89@gmail.com>
2018-03-11 00:23:04 +0530

    Update numbers.rb

--- numbers.rb
+++ numbers.rb
@@ -4,13 +4,20 @@
  Usual operators:  
   + addition      
   - subtraction      
-  * multiplication  
-  / division  
-=end  
+  * multiplication
+  / division 
+=end
   
-puts 3 + 5  # prints  8
-puts 2 * 3  # prints  6
+#Addition 
+puts 3 + 5 # print 8
+
+#Subtraction
+puts 7 - 2  # print 5
+
+#Multiplication
+puts 2 * 3  # print 6
+
 # Integer division  
 # When you do arithmetic with integers, you'll get integer answers  
-puts 5 / 2   # prints  2
-puts 5 - 7   # prints  -2
+puts 5 / 2   # print 2
+
