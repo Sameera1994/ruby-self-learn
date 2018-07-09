ee92771b0b2b6915656d8672ae2606ef1cde0c74
Sameera Lakshitha <lakshithasameera89@gmail.com>
2018-07-10 00:19:51 +0530

    difference-between-print-and-puts

--- print.rb
+++ print.rb
@@ -3,3 +3,14 @@ print 'Enter you want to display'
 #you can use puts for output
 
 puts 'Hello World!' #Hello World!
+
+##difference-between-print-and-puts
+
+print [nil, 1, 2] #[nil, 1, 2]
+
+puts [nil, 1, 2] 
+
+#result
+#1
+#2
+
